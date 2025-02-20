f7609b6ae7684eefbb8266bc62a203d157ed5510
JoaquinDS <joaquin.hz.p@outlook.com>
2025-02-20 16:19:57 -0600

    Base project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import Pagination from "@/components/Pagination";
 import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
+<<<<<<< HEAD
   const [error, setError] = useState<string | null>(null);
   const [data, setData] = useState<PokemonInterface[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
@@ -98,6 +99,13 @@ export default function Home() {
       )}
 
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+=======
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
+      <main></main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+>>>>>>> e4b133d (Base project)
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -143,7 +151,12 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+<<<<<<< HEAD
       </footer> */}
+=======
+      </footer>
+
+>>>>>>> e4b133d (Base project)
     </div>
   );
 }
